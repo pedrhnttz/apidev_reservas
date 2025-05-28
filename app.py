10a6d0fde58540edab76a223eb8ea72ab17a5d5b
PEDRO SANTOS <pedrohenos600@gmail.com>
2025-05-28 18:42:14 -0300

    add requirements.txt

--- app.py
+++ app.py
@@ -1,16 +1,11 @@
-from flask import Flask
-from database import db
-from reserva_route import routes
+from config import app, db
+from controllers.reserva_route import reserva_bp
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///reservas.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.register_blueprint(reserva_bp)
 
 db.init_app(app)
-app.register_blueprint(routes)
-
 with app.app_context():
     db.create_all()
 
-if __name__ == "__main__":
-    app.run(port=5001, debug=True)
+if __name__ == '__main__':
+    app.run(host=app.config["HOST"], port = app.config['PORT'],debug=app.config['DEBUG'] )
