faeb89cdb8f97bc006b9a770812d321dc99803d9
PEDRO SANTOS <pedrohenos600@gmail.com>
2025-05-28 21:44:24 -0300

    metodo post, criacao das funcoes para validar os dados enviados pelo usuario

--- models/reserva_model.py
+++ models/reserva_model.py
@@ -1,8 +1,9 @@
-from flask import jsonify
 from config import db
+from flask import jsonify
+from datetime import date, datetime
 import requests
 
-main_data = "http://127.0.0.1:5000/api"
+main_api = "http://127.0.0.1:5000/api"
 
 class Reserva(db.Model):
     __tablename__ = 'reservas'
@@ -49,15 +50,83 @@ def get_reserva_by_id(id):
         raise ReservaNotFound
     return reserva.to_dict()
 
-def validar_turma(id):
-    r = requests.get(f"{main_data}/turmas/{id}")
-    return r.status_code == 200
+def create_reserva(reserva):
+    is_valid, error_msg = validar_reserva(reserva)
+    if not is_valid:
+        return jsonify({'erro': error_msg}), 400
+    
+    reserva_data = reserva.get('data')
+    data_obj = datetime.strptime(reserva_data, "%Y-%m-%d").date()
 
-def create_reserva():
-    pass
+    nova_reserva = Reserva(
+        turma_id = reserva.get('turma_id'),
+        sala = reserva.get('sala'),
+        data = data_obj,
+        hora_inicio = reserva.get('hora_inicio'),
+        hora_fim = reserva.get('hora_fim')
+    )
+
+    db.session.add(nova_reserva)
+    db.session.commit()
 
 def update_reserva():
     pass
 
 def delete_reserva():
     pass
+
+# Funções secundárias
+
+def validar_reserva(dados):
+    required_fields = {
+        "turma_id": "ID da turma",
+        "sala": "Sala",
+        "data": "Data",
+        "hora_inicio": "Horário de início",
+        "hora_fim": "Horário de término"
+    }
+
+    for field, display_name in required_fields.items():
+        if field not in dados or not dados[field]:
+            return False, f"O campo '{display_name}' é obrigatório."
+        
+    turma_id = dados.get('turma_id')
+    if not validar_turma(turma_id):
+        return False, f'Turma não encontrada.'
+    
+    data_reserva = dados.get('data')
+    if not validar_data(data_reserva):
+        return False, "O campo 'Data' possui um formato inválido. Use AAAA-MM-DD."
+    
+    hora_inicio = dados.get('hora_inicio')
+    hora_fim = dados.get('hora_fim')
+    is_horas_valid, horas_error_msg = validar_hora(hora_inicio, hora_fim)
+    if not is_horas_valid:
+        return False, horas_error_msg
+    
+    return True, None
+
+def validar_turma(turma_id):
+    try:
+        r = requests.get(f"{main_api}/turmas/{turma_id}")
+        return r.status_code == 200
+    except requests.exceptions.ConnectionError:
+        return False
+
+def validar_data(data_str, formato="%Y-%m-%d"):
+    try:
+        datetime.strptime(data_str, formato)
+        return True
+    except ValueError:
+        return False
+
+def validar_hora(hora_inicio, hora_fim, formato="%H:%M"):
+    try:
+        dt_hora_inicio = datetime.strptime(hora_inicio, formato).time()
+        dt_hora_fim = datetime.strptime(hora_fim, formato).time()
+    except ValueError:
+        return False, f"O formato das horas deve ser '{formato}' (ex: 09:00)."
+    if dt_hora_inicio >= dt_hora_fim:
+        return False, "A 'Hora de início' deve ser anterior à 'Hora de término'."
+    return True, None
+
